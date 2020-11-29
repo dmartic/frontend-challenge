a4d0c93d21fc882b1e410691f787d467e29cea5c
Daniel Martic <dmartic90@gmail.com>
2020-11-29 18:21:44 +0100

    Added 'creates shortened URL' test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders App', () => {
@@ -7,3 +7,16 @@ test('renders App', () => {
     const punchline = screen.getByText(/Change mobility for good/i);
     expect(punchline).toBeInTheDocument();
 });
+
+test('creates shortened URL', () => {
+    render(<App />);
+
+    const input = screen.getByPlaceholderText(/Url to shorten/i);
+    const submit = screen.getByText(/Shorten and copy URL/i);
+    const copiedText = screen.findByText(/--- copied!/i)
+
+    fireEvent.change(input, {target: {value: "https://www.google.com"}});
+    fireEvent.click(submit);
+
+    expect(copiedText);
+})
